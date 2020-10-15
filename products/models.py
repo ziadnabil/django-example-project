23b97d2e4dece87037229a3b283bec1ed1f52b96
ziadnabil <ziadmoh2525@gmail.com>
2020-10-15 23:30:15 +0200

    home view

--- products/models.py
+++ products/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    id = models.AutoField()
     name = models.CharField(max_length=200)
     price = models.IntegerField(default=0)
     description = models.TextField(null=True, blank=True)
--- users/models.py
+++ users/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField()
-    email = models.CharField()
-    password = models.CharField()
+    name = models.CharField(max_length=25)
+    email = models.CharField(max_length=25)
+    password = models.CharField(max_length=25)
