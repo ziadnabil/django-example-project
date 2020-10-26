dce691ded2bc63e82ded897dbd54c4fdcfb159b9
ziadnabil <ziadmoh2525@gmail.com>
2020-10-26 23:44:11 +0200

    adding accounts app and forms for login

---
+++ accounts/forms.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import get_user_model
+from django import forms
+
+# check for unique email & username
+
+User = get_user_model()
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "id": "user-password",
+            }
+        )
+    )
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        qs = User.objects.filter(username__iexact=username)
+        if not qs.exists():
+            raise forms.ValidationError("Invalid user")
+        return username
