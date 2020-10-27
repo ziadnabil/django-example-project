b78572edfa679b9737c81b4b848679cd3eab2dc4
ziadnabil <ziadmoh2525@gmail.com>
2020-10-27 23:41:57 +0200

    adding login_view and authentication

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,6 +17,10 @@ class LoginForm(forms.Form):
         )
     )
 
+    # def clean(self):
+    #     username = self.cleaned_data.get("username")
+    #     password = self.cleaned_data.get("password")
+
     def clean_username(self):
         username = self.cleaned_data.get("username")
         qs = User.objects.filter(username__iexact=username)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import auth, authenticate, login
+from django.shortcuts import redirect, render
+from .forms import LoginForm
 
 # Create your views here.
+
+
+def login_view(request):
+    form = LoginForm(request.Post or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        else:
+            # attempt = request.session.get("attempt") or 0
+            # request.session["attempt"] = attempt + 1
+            # return redirect("/invalied-password")
+            request.session["invalid_user"] = 1
+
+    return render(request, "forms.html", {"form": form})
